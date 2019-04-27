53dde02a66b2b7c945a44d39b557ac6979115b0d
Alexa Drenick <adrenick@calpoly.edu>
2019-04-27 13:10:57 -0700

    mountain ranges and valleys

--- resources/terrain_frag.glsl
+++ resources/terrain_frag.glsl
@@ -5,6 +5,8 @@ uniform vec3 MatDif;
 uniform vec3 MatSpec;
 uniform float shine;
 
+//uniform sampler2D Texture;
+
 in vec3 fragNor;
 in vec3 iLightVec;
 in vec3 HVec;
@@ -15,7 +17,6 @@ in vec4 pos;
 
 layout(location = 0) out vec4 color;
 
-
 void main()
 {
     vec3 norm = fragNor;
@@ -24,9 +25,12 @@ void main()
     vec3 fdy = vec3(dFdy(pos.x), dFdy(pos.y), dFdy(pos.z));
     norm = normalize(cross(fdx, fdy));
 
+    //vec2 vTexCoord = vec2(pos.x, pos.z)/100;
+    //vec4 texColor = texture(Texture, vTexCoord);
+
     vec3 ambient = MatAmb;
 
-    vec3 diffuse = MatDif*(clamp(dot(norm, iLightVec), 0, 1));
+    vec3 diffuse = MatDif*(clamp(dot(norm, iLightVec), 0, 1));//*texColor.xyz;
 
     vec3 specular = MatSpec*pow(clamp(dot(HVec, norm), 0, 1), shine);
 
@@ -35,7 +39,7 @@ void main()
     if (water == 1.0){
         color = vec4(phong, 1.0)*vec4(0.216, 0.663, 0.835, 0.25);
     } else {
-        color = vec4(phong, 1.0)*vec4(0.192, 0.192, 0.192, 1.0);
+        color = vec4(phong, 1.0);
     }
 
 }
--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -6,6 +6,7 @@ Terrain::Terrain()
     octave = 0;
     freq = 1.0;
     power = 0.5;
+    vertexArrayID = 0;
 }
 
 void Terrain::initPermTexture(GLuint *ID)
@@ -36,10 +37,19 @@ void Terrain::initPermTexture(GLuint *ID)
     glActiveTexture(GL_TEXTURE0);
 }
 
+void Terrain::initTex()
+{
+    terrainTexture = std::make_shared<Texture>();
+    terrainTexture->setFilename("../resources/Onefacegrey.jpg");
+    terrainTexture->init();
+    terrainTexture->setUnit(0);
+    terrainTexture->setWrapModes(GL_REPEAT, GL_REPEAT);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 void Terrain::render(glm::mat4 const & P, glm::mat4 const & V, glm::mat4 const & M, glm::vec3 cameraPos)
 {
     prog->bind();
-
 
 
     glActiveTexture(GL_TEXTURE1);
@@ -51,16 +61,22 @@ void Terrain::render(glm::mat4 const & P, glm::mat4 const & V, glm::mat4 const &
     glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, glm::value_ptr(M));
     glUniform3f(prog->getUniform("cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
     glUniform3f(prog->getUniform("lightPos"), -2.0, 2.0, 2.0); // TODO
+    //glUniform3f(prog->getUniform("lightPos"), 0.0, 0.0, 0.0); // TODO
+
+    //terrainTexture->bind(prog->getUniform("Texture"));
 
     glUniform1i(prog->getUniform("octave"), octave);
     glUniform1f(prog->getUniform("freq"), freq);
     glUniform1f(prog->getUniform("power"), power);
 
     // flat grey
+
     glUniform3f(prog->getUniform("MatAmb"), 0.13, 0.13, 0.14);
+    //glUniform3f(prog->getUniform("MatAmb"), 0.23, 0.23, 0.24);
     glUniform3f(prog->getUniform("MatDif"), 0.3, 0.3, 0.4);
     glUniform3f(prog->getUniform("MatSpec"), 0.3, 0.3, 0.4);
     glUniform1f(prog->getUniform("shine"), 4.0);
+
 
     // pearl
     /*
@@ -72,18 +88,149 @@ void Terrain::render(glm::mat4 const & P, glm::mat4 const & V, glm::mat4 const &
 
     draw();
 
+    //terrainTexture->unbind();
     prog->unbind();
+}
+
+void Terrain::computeIndicesForClipVolume(float clipx0, float clipx1, float clipz0, float clipz1)
+{
+    //std::cout << "81" << std::endl;
+    int minCol = std::max(std::min((clipx0 / (float)STEP), (float)(WIDTH - 2)), 0.f);
+    int maxCol = std::max(std::min((clipx1 +  STEP) / STEP, (float)(WIDTH - 2)), 0.f);
+    int minRow = std::max(std::min(-clipz1 / STEP, (float)(DEPTH - 1)), 0.f);
+    int maxRow = std::max(std::min(-(clipz0 - STEP) / STEP, (float)(DEPTH - 1)), 0.f);
+
+    if (minCol == maxCol || minRow == maxRow)
+    {
+        std::cout << "terrain not visible" << std::endl;
+        numIndices = 0;
+    }
+
+    unsigned index = 0;
+    for (int c = minCol; c < maxCol + 1; c++)
+    {
+        for (int r = minRow; r < maxRow + 1; r++)
+        {
+            // if not first strip, link to previous strip
+            if (c > minCol && r == minRow)
+            {
+                indices[index++] = c * DEPTH + r;
+            }
+            indices[index++] = c * DEPTH + r;
+            indices[index++] = (c + 1) * DEPTH + r;
+
+            // link to next strip
+            if (r == maxRow && c < maxCol)
+            {
+                indices[index++] = (c + 1) * DEPTH + r;
+            }
+        }
+        //std::cout << "indices: " << index << std::endl;
+    }
+    numIndices = index;
+    //std::cout << "114" << std::endl;
+}
+
+void Terrain::bindVAO()
+{
+    //std::cout << "117" << std::endl;
+    if (vertexArrayID == 0) {
+        unsigned numVertices = vertices.size();
+        unsigned vertexByteSize = sizeof(glm::vec3);
+        unsigned vertexFloatSize = vertexByteSize / sizeof(float);
+
+        unsigned bytes = numVertices * vertexByteSize;
+        //uint8_t *vertexData = g_new(uint8_t, bytes);
+        void *vertexData = malloc(bytes);
+        //float *vertexDataCast = malloc(bytes);
+
+        float *vertexDataCast = (float *) vertexData;
+        for (int i = 0; i < numVertices; i++) {
+            memcpy(&vertexDataCast[vertexFloatSize * i], &vertices[i],
+                   vertexByteSize);
+        }
+
+
+        glGenBuffers(1, &vertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glBufferData(GL_ARRAY_BUFFER, bytes, vertexData, GL_STATIC_DRAW);
+        free(vertexData);
+
+        // TODO how does num index buffers affect this
+        /*
+        glGenBuffers(3, &indexBuffers);
+        int size = 251;
+        int byteSize = (((size - 1) * (size * 2) + (size - 2) + (size - 2)) * sizeof(unsigned) * 2); // *2?
+        for (int i = 0; i < 3; i++)
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffers[i]);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, byteSize, i == )
+        }
+         */
+        glGenBuffers(1, &vertexIndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndices * sizeof(int), indices,
+                     GL_DYNAMIC_DRAW);
+
+
+        glGenVertexArrays(1, &vertexArrayID);
+        glBindVertexArray(vertexArrayID);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *) 0);
+    }
+    else
+    {
+        glBindVertexArray(vertexArrayID);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+    }
+
+
 }
 
 
 void Terrain::generateGrid()
 {
+    for (int vx = 0; vx < WIDTH; vx++)
+    {
+        for (int vz = 0; vz < DEPTH; vz++)
+        {
+            // float vy = height[vx * depth + vz);?
+            // #define TERRAIN(t, w, d) t->height[(w) * t->depth + (d)]
+            float vy = 0;
+            glm::vec3 v0 = glm::vec3(vx * STEP, vy, -vz * STEP);
+            // calculate normal?
+            vertices.push_back(v0); // add normal?
+        }
+    }
 
-    int VERTEX_COUNT = 300;
-    int SIZE = 25;
+    unsigned num_indices = (WIDTH - 1) * (DEPTH * 2) + (WIDTH - 2) + (DEPTH - 2);
 
-    GLfloat vertices[300 * 300 * 3];
-    GLint indices[6 * (299) * (299)];
+    /*
+        typedef struct {
+            float x0, x1, y0, y1, z0, z1;
+        } TerClipVolume;
+    */
+    float clipx0 = 0.f;
+    float clipx1 = (WIDTH - 1) * STEP;
+    float clipz1 = 0.f;
+    float clipz0 = -(DEPTH - 1) * STEP;
+
+    //compute indices for clip volume
+    indices = (unsigned *) malloc(num_indices * sizeof(unsigned));
+    computeIndicesForClipVolume(clipx0, clipx1, clipz0, clipz1);
+
+    assert(num_indices == numIndices);
+
+    //std::cout << "209" << std::endl;
+    bindVAO();
+
+    /*
+    GLfloat vertices[VERTEX_COUNT * VERTEX_COUNT * 3];
+    GLint indices[6 * (VERTEX_COUNT-1) * (VERTEX_COUNT-1)];
 
     int vertexPointer = 0;
     for (int i = 0; i < VERTEX_COUNT; i++)
@@ -131,6 +278,7 @@ void Terrain::generateGrid()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
 
     glBindVertexArray(0);
+     */
 }
 
 void Terrain::clean()
@@ -145,8 +293,22 @@ void Terrain::clean()
 
 void Terrain::draw()
 {
+    /*
     glBindVertexArray(vertexArrayID);
-    glDrawElements(GL_TRIANGLES, 300 * 300 * 10, GL_UNSIGNED_INT, nullptr);
+    glDrawElements(GL_TRIANGLES, VERTEX_COUNT * VERTEX_COUNT * 10, GL_UNSIGNED_INT, nullptr);
+    glBindVertexArray(0);
+     */
+    // TODO offset?
+    bindVAO();
+    glDrawElements(GL_TRIANGLE_STRIP, numIndices, GL_UNSIGNED_INT, 0);
+    unbind();
+
+}
+
+void Terrain::unbind()
+{
+    glDisableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
 
@@ -167,6 +329,9 @@ void Terrain::initTerrain()
     prog->addUniform("M");
 
     prog->addUniform("permTexture");
+    //prog->addUniform("Texture");
+
+    //initTex();
 
     prog->addUniform("MatAmb");
     prog->addUniform("MatDif");
