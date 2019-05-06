b3e03f3104b76b1716cb6c6b82284df7fd8a03c9
carolinecullen <cullen.caroline@gmail.com>
2019-05-05 20:34:17 -0700

    Changing window title.

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -47,7 +47,7 @@ bool WindowManager::init(int const width, int const height)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 	// Create a windowed mode window and its OpenGL context.
-	windowHandle = glfwCreateWindow(width, height, "Blender", nullptr, nullptr);
+	windowHandle = glfwCreateWindow(width, height, "obtain.", nullptr, nullptr);
 	if (! windowHandle)
 	{
 		glfwTerminate();
