25f60bf0198d6f64d1c939c6c443ef00aa38c894
Alexa Drenick <adrenick@calpoly.edu>
2019-06-10 18:25:40 -0700

    the ferns arent green and i quit{

--- resources/comb_frag.glsl
+++ resources/comb_frag.glsl
@@ -9,10 +9,18 @@ out vec4 Outcolor;
 
 void main() {
 
+    //const float gamma = 2.2;
+    //float exposure = 1.0;
     vec3 bloomCol = texture(bloomBuf, texCoord).rgb;
     vec3 sceneCol = texture(sceneBuf, texCoord).rgb;
 
-    Outcolor = vec4(bloomCol + sceneCol, 1.0);
+    vec3 color = bloomCol + sceneCol;
+
+    //vec3 result = vec3(1.0) - exp(-color * exposure);
+
+    //result = pow(result, vec3(1.0 / gamma));
+
+    Outcolor = vec4(color, 1.0);
 
     //Outcolor = vec4(sceneCol, 1.0);
 }
